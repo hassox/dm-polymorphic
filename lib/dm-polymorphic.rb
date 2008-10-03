bfdf65bf9a84acf821948564445ec631f74fddc2
Wayne E. Seguin <wayneeseguin@gmail.com>
2008-10-03 14:16:54 -0400

    s/Class/Klass/g.

--- lib/dm-polymorphic.rb
+++ lib/dm-polymorphic.rb
@@ -11,7 +11,7 @@ module DataMapper
       
       def is_polymorphic(name, id_type=String)
         self.class_eval <<-EOS, __FILE__, __LINE__
-          property :"#{name}_class", Class
+          property :"#{name}_class", Klass
           property :"#{name}_id",    #{id_type}
           
           def #{name}
