e3f43816d4e60f706f98fa50467c637a7ba30411
Ripta Pasay <github@r8y.org>
2009-10-04 05:41:51 +0800

    DM::Associations is now DM::Model::Relationship

--- lib/associations.rb
+++ lib/associations.rb
@@ -1,22 +1,25 @@
 module DataMapper
-  module Associations
-  
-    alias_method :has_without_polymorphism, :has
-    
-    def has(cardinality, name, opts = {})
-      if interface = opts.delete(:via)
-        
-        # get the child model
-        child_model_name =  opts.fetch(:class_name, Extlib::Inflection.classify(name))
-        child_klass =       Extlib::Inflection.constantize(child_model_name)
-        belongs_to_name =   Extlib::Inflection.underscore(Extlib::Inflection.demodulize(self.name))
+  module Model
+    module Relationship
       
-        # Setup the has with the right stuff
-        has_without_polymorphism cardinality, name,   :child_key => [:"#{interface}_id"], child_klass.send(:"#{interface}_class") => self
-        child_klass.belongs_to :"#{belongs_to_name}", :child_key => [:"#{interface}_id"], self.send(:"#{name}").send(:"#{interface}_class") => self
-      else
-        has_without_polymorphism(cardinality, name, opts)
+      alias_method :has_without_polymorphism, :has
+      
+      def has(cardinality, name, opts = {})
+        if interface = opts.delete(:via)
+        
+          # get the child model
+          child_model_name =  opts.fetch(:class_name, Extlib::Inflection.classify(name))
+          child_klass =       Extlib::Inflection.constantize(child_model_name)
+          belongs_to_name =   Extlib::Inflection.underscore(Extlib::Inflection.demodulize(self.name))
+          
+          # Setup the has with the right stuff
+          has_without_polymorphism cardinality, name,   :child_key => [:"#{interface}_id"], child_klass.send(:"#{interface}_class") => self
+          child_klass.belongs_to :"#{belongs_to_name}", :child_key => [:"#{interface}_id"], self.send(:"#{name}").send(:"#{interface}_class") => self
+        else
+          has_without_polymorphism(cardinality, name, opts)
+        end
       end
+      
     end
   end
 end
