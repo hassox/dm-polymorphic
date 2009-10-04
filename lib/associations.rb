9332451c2811029d950abd5da53912842f2ff827
Ripta Pasay <github@r8y.org>
2009-10-04 08:13:39 +0800

    Directly use a symbol instead of a property for the conditions on the relationship

--- lib/associations.rb
+++ lib/associations.rb
@@ -6,15 +6,14 @@ module DataMapper
       
       def has(cardinality, name, opts = {})
         if interface = opts.delete(:polymorphically)
-        
           # get the child model
           child_model_name =  opts.fetch(:class_name, Extlib::Inflection.classify(name))
           child_klass =       Extlib::Inflection.constantize(child_model_name)
           belongs_to_name =   Extlib::Inflection.underscore(Extlib::Inflection.demodulize(self.name))
           
           # Setup the has with the right stuff
-          has_without_polymorphism cardinality, name,   :child_key => [:"#{interface}_id"], child_klass.send(:"#{interface}_class") => self
-          child_klass.belongs_to :"#{belongs_to_name}", :child_key => [:"#{interface}_id"], self.send(:"#{name}").send(:"#{interface}_class") => self
+          has_without_polymorphism cardinality, name,   :child_key => [:"#{interface}_id"], :"#{interface}_class" => self
+          child_klass.belongs_to :"#{belongs_to_name}", :child_key => [:"#{interface}_id"], :"#{interface}_class" => self
         else
           has_without_polymorphism(cardinality, name, opts)
         end
@@ -23,4 +22,3 @@ module DataMapper
     end
   end
 end
-    
