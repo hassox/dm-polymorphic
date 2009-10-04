65b575a9f968c5bc62164617392986e8f5527081
Ripta Pasay <github@r8y.org>
2009-10-04 10:47:25 +0800

    #has should take any number of arguments

--- lib/associations.rb
+++ lib/associations.rb
@@ -4,7 +4,8 @@ module DataMapper
       
       alias_method :has_without_polymorphism, :has
       
-      def has(cardinality, name, opts = {})
+      def has(cardinality, name, *args)
+        opts = args.extract_options!
         if interface = opts.delete(:polymorphically)
           # get the child model
           child_model_name =  opts.fetch(:class_name, Extlib::Inflection.classify(name))
@@ -15,7 +16,7 @@ module DataMapper
           has_without_polymorphism cardinality, name,   :child_key => [:"#{interface}_id"], :"#{interface}_class" => self
           child_klass.belongs_to :"#{belongs_to_name}", :child_key => [:"#{interface}_id"], :"#{interface}_class" => self
         else
-          has_without_polymorphism(cardinality, name, opts)
+          has_without_polymorphism(cardinality, name, args + [opts])
         end
       end
       
