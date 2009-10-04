ad4afd7019a222fee798420ef32b54f6c0ac3328
Ripta Pasay <github@r8y.org>
2009-10-04 08:14:34 +0800

    Remove failing code; "primitive Class" takes care of everything

--- lib/types.rb
+++ lib/types.rb
@@ -2,22 +2,6 @@ module DataMapper
   module Types
     class Klass < DataMapper::Type
       primitive Class
-
-      def self.load(value, property)
-        if value
-          value # value.is_a?(Class) ? value : Extlib::Inflection.constantize(value)
-        else
-          nil
-        end
-      end
-
-      def self.dump(value, property)
-        if value
-          (value.is_a? Class) ? value.name : Extlib::Inflection.constantize(value.to_s)
-        else
-          nil
-        end
-      end
-    end # class URI
+    end # class Klass
   end # module Types
 end # module DataMapper
