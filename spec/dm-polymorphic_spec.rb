8b2cb7c655c72aa42aa4ef44cd84c46b8dd9d619
Ripta Pasay <github@r8y.org>
2009-10-04 09:42:30 +0800

    Spec changes

--- spec/dm-polymorphic_spec.rb
+++ spec/dm-polymorphic_spec.rb
@@ -48,11 +48,20 @@ if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
       end
       
       it "should add the comment with the << syntax for #{klass}" do
-        item = klass.create(:name => "item2")      
+        item = klass.create(:name => "item2")
         c = Comment.new(:text => "comment2")
         item.comments << c
+        c.save
         item.reload
         item.comments(:text => "comment2").should have(1).item
+      end
+      
+      it "should not add the comment with the << syntax for #{klass} if comment was not saved" do
+        item = klass.create(:name => "item2a")      
+        c = Comment.new(:text => "comment2a")
+        item.comments << c
+        item.reload
+        item.comments(:text => "comment2a").should have(0).item
       end
       
       it "should access all the comments from the post for #{klass}" do
@@ -60,6 +69,7 @@ if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
         c1 = Comment.new(:text => "comment3")
         c2 = Comment.new(:text => "comment4")
         [c1,c2].each{|c| item.comments << c}
+        item.comments.save
         item.reload
         item.comments.should have(2).items
       end
@@ -76,34 +86,35 @@ if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
         item = klass.create(:name => "item5")        
         c = Comment.new(:text => "comment6")
         item.comments << c
+        c.save
         item.reload; c.reload
         c.commentable.should == item
       end
     end
     
-    it "should only make one query to each Model" do
-      post1 = Post.create(:name => "post1")
-      article1 = Article.create(:name => "article1")
-      post2 = Post.create(:name => "post2")
-      article2 = Article.create(:name => "article2")
-      
-      %w(one two three four five six seven eight nine ten).each_with_index do |text, index|
-        [post1, post2, article1, article2].each do |item|
-          item.comments << Comment.new(:text => text)
-        end
-      end
-      
-      Comment.all.each do |c|
-        puts "COMMENT: #{c.text} - #{c.commentable_class}"
-      end
-      
-      Post.should_receive(:all).once
-      # Article.should_receive(:all).once.and_return(Article.comments)
-    
-      Comment.all.each do |comment|
-        [Post, Article].should include(comment.commentable.class)
-      end
-    end
+    # it "should only make one query to each Model" do
+    #   post1 = Post.create(:name => "post1")
+    #   article1 = Article.create(:name => "article1")
+    #   post2 = Post.create(:name => "post2")
+    #   article2 = Article.create(:name => "article2")
+    #   
+    #   %w(one two three four five six seven eight nine ten).each_with_index do |text, index|
+    #     [post1, post2, article1, article2].each do |item|
+    #       item.comments.create(:text => text)
+    #     end
+    #   end
+    #   
+    #   Post.all.each do |c|
+    #     puts "COMMENT: #{c.text} - #{c.commentable_class}"
+    #   end
+    #   
+    #   Post.should_receive(:all).once
+    #   Article.should_receive(:all).once.and_return(Article.comments)
+    #   
+    #   Comment.all.each do |comment|
+    #     [Post, Article].should include(comment.commentable.class)
+    #   end
+    # end
 
   end
 end
