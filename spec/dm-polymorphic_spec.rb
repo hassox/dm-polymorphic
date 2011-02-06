447a4c399ac18201a6f4602dd8584766efd41125
Mike Fulcher <mike@plan9design.co.uk>
2011-02-06 13:03:20 +0000

    Update specs

--- spec/dm-polymorphic_spec.rb
+++ spec/dm-polymorphic_spec.rb
@@ -2,135 +2,120 @@ require File.dirname(__FILE__) + "/spec_helper"
 
 DataMapper::Logger.new(STDOUT, :debug)
 
-if HAS_SQLITE3 || HAS_MYSQL || HAS_POSTGRES
-  describe "DataMapper::Polymorphic" do
+describe "DataMapper::Polymorphic" do
 
-    [:original, :new].each do |syntax|
-      SYNTAX = syntax
+  class Comment
+    include DataMapper::Resource
 
-      class Comment
-        include DataMapper::Resource
+    property :id,   Serial
+    property :text, String
 
-        property :id,   Serial
-        property :text, String
+    belongs_to :commentable, :polymorphic => true
+  end
 
-        is :polymorphic, :commentable                         if SYNTAX == :original
-        belongs_to :commentable, :polymorphic => true         if SYNTAX == :new
-      end
+  class Post
+    include DataMapper::Resource
 
-      class Post
-        include DataMapper::Resource
+    property :id,   Serial
+    property :name, String  
 
-        property :id,   Serial
-        property :name, String  
+    has n, :comments, :as => :commentable
+  end
 
-        has n, :comments, :polymorphically => :commentable    if SYNTAX == :original
-        has n, :comments, :as => :commentable                 if SYNTAX == :new
-      end
+  class Article
+    include DataMapper::Resource
 
-      class Article
-        include DataMapper::Resource
+    property :id,   Serial
+    property :name, String  
 
-        property :id,   Serial
-        property :name, String  
+    has n, :comments, :as => :commentable
+  end
 
-        has n, :comments, :polymorphically => :commentable    if SYNTAX == :original
-        has n, :comments, :as => :commentable                 if SYNTAX == :new
-      end
-  
-      before :each do
-        Comment.auto_migrate!
-        Post.auto_migrate!
-        Article.auto_migrate!
-      end
-  
-      [Article, Post].each do |klass|
-        it "should create an associated object for #{klass}" do
-          item = klass.create(:name => "item1")
-          item.reload
-          item.comments.create(:text => "A Comment")
-          item.reload
-          item.comments(:text => "A Comment").should have(1).item
-        end
-    
-        it "should add the comment with the << syntax for #{klass}" do
-          item = klass.create(:name => "item2")
-          c = Comment.new(:text => "comment2")
-          item.comments << c
-          c.save
-          item.reload
-          item.comments(:text => "comment2").should have(1).item
-        end
-    
-        it "should not add the comment with the << syntax for #{klass} if comment was not saved" do
-          item = klass.create(:name => "item2a")      
-          c = Comment.new(:text => "comment2a")
-          item.comments << c
-          item.reload
-          item.comments(:text => "comment2a").should have(0).item
-        end
-    
-        it "should access all the comments from the post for #{klass}" do
-          item = klass.create(:name => "item3")
-          c1 = Comment.new(:text => "comment3")
-          c2 = Comment.new(:text => "comment4")
-          [c1,c2].each{|c| item.comments << c}
-          item.comments.save
-          item.reload
-          item.comments.should have(2).items
-        end
+  [Article, Post].each do |klass|
+    it "should create an associated object for #{klass}" do
+      item = klass.create(:name => "item1")
+      item.reload
+      item.comments.create(:text => "A Comment")
+      item.reload
+      item.comments(:text => "A Comment").should have(1).item
+    end
 
-        it "should access the commentable from the comment for #{klass}" do
-          item = klass.create(:name => "item5")        
-          c = Comment.new(:text => "comment6")
-          item.comments << c
-          c.save
-          item.reload; c.reload
-          c.commentable.should == item
-        end
-      end
+    it "should add the comment with the << syntax for #{klass}" do
+      item = klass.create(:name => "item2")
+      c = Comment.new(:text => "comment2")
+      item.comments << c
+      c.save
+      item.reload
+      item.comments(:text => "comment2").should have(1).item
+    end
 
-      it "should access the post and article from the comment only if it is a post or article" do
-        article = Article.create(:name => "item6")        
-        post = Post.create(:name => "item7")
-        c1 = Comment.new(:text => "comment6")
-        c2 = Comment.new(:text => "comment7")        
-        post.comments << c1
-        post.save
-        article.comments << c2
-        article.save
-        [post, article, c1, c2].map(&:reload)
-        c1.post.should == post
-        c1.article.should be_nil
-        c2.post.should be_nil
-        c2.article.should == article
-      end  
-        
-      # it "should only make one query to each Model" do
-      #   post1 = Post.create(:name => "post1")
-      #   article1 = Article.create(:name => "article1")
-      #   post2 = Post.create(:name => "post2")
-      #   article2 = Article.create(:name => "article2")
-      #   
-      #   %w(one two three four five six seven eight nine ten).each_with_index do |text, index|
-      #     [post1, post2, article1, article2].each do |item|
-      #       item.comments.create(:text => text)
-      #     end
-      #   end
-      #   
-      #   Post.all.each do |c|
-      #     puts "COMMENT: #{c.text} - #{c.commentable_class}"
-      #   end
-      #   
-      #   Post.should_receive(:all).once
-      #   Article.should_receive(:all).once.and_return(Article.comments)
-      #   
-      #   Comment.all.each do |comment|
-      #     [Post, Article].should include(comment.commentable.class)
-      #   end
-      # end
-      
+    it "should not add the comment with the << syntax for #{klass} if comment was not saved" do
+      item = klass.create(:name => "item2a")      
+      c = Comment.new(:text => "comment2a")
+      item.comments << c
+      item.reload
+      item.comments(:text => "comment2a").should have(0).item
+    end
+
+    it "should access all the comments from the post for #{klass}" do
+      item = klass.create(:name => "item3")
+      c1 = Comment.new(:text => "comment3")
+      c2 = Comment.new(:text => "comment4")
+      [c1,c2].each{|c| item.comments << c}
+      item.comments.save
+      item.reload
+      item.comments.should have(2).items
+    end
+
+    it "should access the commentable from the comment for #{klass}" do
+      item = klass.create(:name => "item5")        
+      c = Comment.new(:text => "comment6")
+      item.comments << c
+      c.save
+      item.reload; c.reload
+      c.commentable.should == item
     end
   end
+
+  it "should access the post and article from the comment only if it is a post or article" do
+    article = Article.create(:name => "item6")        
+    post = Post.create(:name => "item7")
+    c1 = Comment.new(:text => "comment6")
+    c2 = Comment.new(:text => "comment7")        
+    post.comments << c1
+    post.save
+    article.comments << c2
+    article.save
+    [post, article, c1, c2].map(&:reload)
+    c1.post.should == post
+    c1.article.should be_nil
+    c2.post.should be_nil
+    c2.article.should == article
+  end  
+    
+  # it "should only make one query to each Model" do
+  #   post1 = Post.create(:name => "post1")
+  #   article1 = Article.create(:name => "article1")
+  #   post2 = Post.create(:name => "post2")
+  #   article2 = Article.create(:name => "article2")
+  #   
+  #   %w(one two three four five six seven eight nine ten).each_with_index do |text, index|
+  #     [post1, post2, article1, article2].each do |item|
+  #       item.comments.create(:text => text)
+  #     end
+  #   end
+  #   
+  #   Post.all.each do |c|
+  #     puts "COMMENT: #{c.text} - #{c.commentable_class}"
+  #   end
+  #   
+  #   Post.should_receive(:all).once
+  #   Article.should_receive(:all).once.and_return(Article.comments)
+  #   
+  #   Comment.all.each do |comment|
+  #     [Post, Article].should include(comment.commentable.class)
+  #   end
+  # end
+    
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,31 +1,17 @@
-require "rubygems"
-require "pathname"
+require 'rubygems'
+require 'rake'
+require 'rspec'
 
-gem "dm-core", ">=0.10.0"
-require "dm-core"
+require 'dm-core'
+require 'dm-migrations'
+require 'dm-sqlite-adapter'
 
-spec_dir_path = Pathname(__FILE__).dirname.expand_path
-require spec_dir_path.parent + "lib/dm-polymorphic"
+require 'active_support'
 
-def load_driver(name, default_uri)
-  return false if ENV["ADAPTER"] != name.to_s
+DataMapper.setup(:default, 'sqlite::memory:')
 
-  lib = "do_#{name}"
+require File.join(File.dirname(__FILE__), '..', 'lib/dm-polymorphic.rb')
 
-  begin
-    gem lib, ">=0.10.0"
-    require lib
-    DataMapper.setup(name, ENV["#{name.to_s.upcase}_SPEC_URI"] || default_uri)
-    DataMapper::Repository.adapters[:default] =  DataMapper::Repository.adapters[name]
-    true
-  rescue Gem::LoadError => e
-    warn "Could not load #{lib}: #{e}"
-    false
-  end
+Rspec.configure do |config|
+  config.before(:each) { DataMapper.auto_migrate! }
 end
-
-ENV["ADAPTER"] ||= "sqlite3"
-
-HAS_SQLITE3  = load_driver(:sqlite3,  "sqlite3::memory:")
-HAS_MYSQL    = load_driver(:mysql,    "mysql://localhost/dm_core_test")
-HAS_POSTGRES = load_driver(:postgres, "postgres://postgres@localhost/dm_core_test")
